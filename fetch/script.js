83c3bdd6ebf243df150f8339c2e8a7ed30981f7f
Jenya <113167825+Genya10@users.noreply.github.com>
2024-10-08 12:53:52 +0300

    add tasks with Fetch

---
+++ fetch/script.js
@@ -0,0 +1,39 @@
+const nameUser = document.querySelector('h2')
+const emailUser = document.querySelector('h3')
+const cityUser = document.querySelector('.city')
+const streetUser = document.querySelector('.street')
+const suiteUser = document.querySelector('.suite')
+
+fetch('https://jsonplaceholder.typicode.com/users/1')
+.then(response => response.json())
+.then(data => {
+    const {name, email, address:{city, street, suite}} = data
+
+    nameUser.textContent = `Name user: ${name}`
+    emailUser.textContent = `Email user: ${email}`
+    cityUser.textContent = `City: ${city}`
+    streetUser.textContent = `Street: ${street}`
+    suiteUser.textContent = `Suite: ${suite}`
+    console.log(data)
+})
+.catch(error => {
+    console.error('Error', error)
+})
+
+///////////////////////////////////////////////
+let ulElement = document.querySelector('ul')
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(posts => {
+    ulElement.innerHTML = ''
+
+    posts.forEach(post => {
+        const li = document.createElement('li')
+        li.textContent = post.title
+        ulElement.appendChild(li)
+    })
+})
+.catch(error => {
+    console.error( 'Error', error)
+})
