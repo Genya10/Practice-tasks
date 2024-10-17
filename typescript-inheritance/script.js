35793bf8d48f21c6d804da90bbe938ad37ed5abc
Jenya <113167825+Genya10@users.noreply.github.com>
2024-10-17 20:39:24 +0300

    create ElementHTML, ElementHTMLCSS and few styles

--- typescript-inheritance/script.js
+++ typescript-inheritance/script.js
@@ -33,3 +33,49 @@ class User3 extends User2 {
 }
 const user3 = new User3('Vasya', 'zxcvbn', 28);
 console.log(user3.showInfo());
+//////////////////////////
+class ElementHTML {
+    constructor(a, tag) {
+        this.text = a;
+        this.tag = tag;
+    }
+    render() {
+        const div = document.createElement(this.tag);
+        div.textContent = this.text;
+        return div;
+    }
+}
+const div_1 = new ElementHTML('hello', 'h1');
+document.body.append(div_1.render());
+const manyDiv = [
+    new ElementHTML('bye', 'h2'),
+    new ElementHTML('cross', 'div'),
+    new ElementHTML('full', 'h3'),
+    new ElementHTML('broke', 'b')
+];
+manyDiv.forEach(item => {
+    document.body.append(item.render());
+});
+////////////////////////////////
+class ElementHTMLCSS extends ElementHTML {
+    constructor(a, tag, b) {
+        super(a, tag);
+        this.cssClass = b;
+    }
+    render() {
+        const div = super.render();
+        this.cssClass.forEach(item => div.classList.add(item));
+        return div;
+    }
+}
+const div_2 = new ElementHTMLCSS('div with css', 'div', ['red', 'bold']);
+document.body.append(div_2.render());
+const divArray = [
+    new ElementHTMLCSS('title', 'h2', ['uppercase', 'green']),
+    new ElementHTMLCSS('span', 'span', ['blue']),
+    new ElementHTMLCSS('this is bold', 'h3', ['yellow']),
+    new ElementHTMLCSS('just div', 'div', ['uppercase']),
+];
+divArray.forEach(item => {
+    document.body.append(item.render());
+});
